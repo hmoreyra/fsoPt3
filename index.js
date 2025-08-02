297f342e5ff21fb86611b4b84a1a383fc9c785e7
Hernán Moreyra <hernan_hello@hotmail.com>
2025-08-01 23:04:56 -0300

    exercise 3.7

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 app.use(express.json()) // to use json parser
+app.use(morgan('tiny'))
 
 let persons = [
     { 
